0306311a09dbc849fbfedaf4dca289590b67bc81
jstensrud <jackstensrud@gmail.com>
2019-03-23 16:42:05 -0400

    add npc and player movement

---
+++ Assets/__Scripts/NPC.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+
+	[Header("Set in Inspector")] 
+	public Material defaultMat;
+	public Material highlightedMat;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			gameObject.GetComponent<Renderer>().material = highlightedMat;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			gameObject.GetComponent<Renderer>().material = defaultMat;
+		}
+	}
+}
