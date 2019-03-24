2d4fd014a2de300c5c569cef9e8384d1d74881bf
JosephZouRHIT <zous@rose-hulman.edu>
2019-03-24 19:32:00 -0400

    Added basic follow cam
    Need a better initial camera position

---
+++ Assets/__Scripts/FollowCam.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+    [Header("Set in Inspector")]
+    public GameObject POI;
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+    [Header("Set Dynamically")]
+    public float camZ;
+
+    private Vector3 distance;
+
+    // Use this for initialization
+    void Start()
+    {
+        distance = transform.position - POI.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (POI == null) return;
+        transform.position = POI.transform.position + distance;
+    }
+}
