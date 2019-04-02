a0578f976c8707647a3eed98d846715f2c7e5aa5
William Graham <grahamw@grahamw-5520.rose-hulman.edu>
2019-04-02 13:12:16 -0400

    Added things

--- Assets/__Scripts/PlayerControllerCylinder.cs
+++ Assets/__Scripts/PlayerControllerCylinder.cs
@@ -6,7 +6,8 @@ using System;
 
 public class PlayerControllerCylinder : MonoBehaviour
 {
-
+    Ray cameraRay;
+    RaycastHit cameraRayHit;
 	[Header("Set in Inspector")] 
 	public float speed = 0.5f;
 	Animator anim;
@@ -27,34 +28,39 @@ public class PlayerControllerCylinder : MonoBehaviour
 
 	void Move()
 	{
-		float hAxis = Input.GetAxis("Horizontal");
-		float vAxis = Input.GetAxis("Vertical");
-		Vector3 pos = new Vector3(-hAxis, 0, -vAxis);
-		pos = pos * speed;
-		pos = Quaternion.Euler(0, 45, 0) * pos;
-		if (hAxis != 0 || vAxis != 0)
-			anim.SetBool("IsWalking", true);
-		else 
-			anim.SetBool("IsWalking", false);
-		transform.position += pos;
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+        Vector3 pos = new Vector3(-hAxis, 0, -vAxis);
+        pos = pos * speed;
+        pos = (transform.rotation * Quaternion.Euler(0, 180, 0)) * pos;
+        if (hAxis != 0 || vAxis != 0)
+            anim.SetBool("IsWalking", true);
+        else
+            anim.SetBool("IsWalking", false);
+        transform.position += pos;
 	}
 
 	void Rotate()
 	{
-		float hAxis = Input.GetAxis("Horizontal");
-		float vAxis = Input.GetAxis("Vertical");
-		if (vAxis != 0){
-			angle = (float) ((Math.Atan(hAxis/vAxis))*(180/Math.PI) + 45);
-			if (vAxis > 0)
-				angle += 180;
-		}
-		else if (hAxis < 0)
-			angle = 135;
-		else if (hAxis > 0)
-			angle = 315;
-		
-		transform.rotation = Quaternion.Euler(0,angle,0);
+    //float hAxis = Input.GetAxis("Horizontal");
+    //float vAxis = Input.GetAxis("Vertical");
+    //if (vAxis != 0){
+    //	angle = (float) ((Math.Atan(hAxis/vAxis))*(180/Math.PI) + 45);
+    //	if (vAxis > 0)
+    //		angle += 180;
+    //}
+    //else if (hAxis < 0)
+    //	angle = 135;
+    //else if (hAxis > 0)
+    //	angle = 315;
+    //
+    //transform.rotation = Quaternion.Euler(0,angle,0);
 
-
-	}
+        cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(cameraRay, out cameraRayHit))
+        {
+            Vector3 targetPosition = new Vector3(cameraRayHit.point.x, transform.position.y, cameraRayHit.point.z);
+            transform.LookAt(targetPosition);
+        }
+    }
 }
